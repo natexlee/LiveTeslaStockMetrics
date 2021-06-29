fd8e915e90213c39958367cc1f3d8c34bc0306d3
Nate Lee <officialnatelee@gmail.com>
2021-06-29 19:30:35 -0400

    Initial Commit

---
+++ TeslaStockApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TeslaStockApp
+//
+//  Created by Nate Lee on 6/29/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TeslaStockApp/TeslaStockAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeslaStockAppApp.swift
+//  TeslaStockApp
+//
+//  Created by Nate Lee on 6/29/21.
+//
+
+import SwiftUI
+
+@main
+struct TeslaStockAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
